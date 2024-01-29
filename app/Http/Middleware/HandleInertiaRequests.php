451749082bc7c4d536ec1e2aaaebc48dac4caf54
ekhorsun <peretsmeshnik@gmail.com>
2024-01-29 15:02:35 +0300

    add and connects Inertia.js to prj

---
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Middleware;
+
+use Illuminate\Http\Request;
+use Inertia\Middleware;
+
+class HandleInertiaRequests extends Middleware
+{
+    protected $rootView = 'app';
+
+    public function version(Request $request): ?string
+    {
+        return parent::version($request);
+    }
+
+    public function share(Request $request): array
+    {
+        return array_merge(parent::share($request), [
+
+        ]);
+    }
+}
---
+++ resources/js/Pages/Index.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+    <header>
+        <h1>INERTIA</h1>
+    </header>
+    <main>
+       <div>INERTIA WORKS</div>
+    </main>
+    <footer>
+        Inertia
+    </footer>
+</template>
